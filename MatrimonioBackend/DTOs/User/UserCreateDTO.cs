06e3686f13ceb2f8c39ba0e68dca67bf98a4cf66
Hindrance <oyvind.reitan3@gmail.com>
2024-06-04 12:43:35 +0200

    oppgraderer user siden jeg nå bruker backend istedet for auth0 for å lage brukere

--- MatrimonioBackend/DTOs/User/UserCreateDTO.cs
+++ MatrimonioBackend/DTOs/User/UserCreateDTO.cs
@@ -2,11 +2,17 @@
 {
     public class UserCreateDTO
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
 
-        public string Username { get; set; }
+        public string? ProfilePicture { get; set; }
 
-        public string Keycloakid { get; set; }
         public string Email { get; set; }
+
+        public string? Nickname { get; set; }
+        public string Password { get; set; }
+
+        public bool Email_Verified { get; set; }
 
 
     }
